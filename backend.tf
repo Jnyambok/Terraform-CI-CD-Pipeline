b4b30982d2dd9444829fe179d99d4edeb362cfd1
Julius Nyambok Nyerere <julius.nyambok@strathmore.edu>
2024-09-30 21:44:46 +0000

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,18 +1,9 @@
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name = "terraform-state-lock"
-  attribute = {
-    name = "state_file_id"
-    type = "S"
+terraform {
+  backend "s3" {
+    bucket = "mystatebucket99"
+    key = "state"   #the folder you want to store it in your bucket
+    region = "eu-north-1"
+    dynamodb_table = "mydynamotable"
   }
-  provisioned_throughput = {
-    read_capacity_units  = 5
-    write_capacity_units = 5
-  }
-}
 
-module "backend_s3" {
-  source = "terraform-aws-modules/s3-bucket"
-  bucket_name = "your-bucket-name"
-  dynamodb_table_name = aws_dynamodb_table.terraform_state_lock.name
-  # ... other configuration
 }
